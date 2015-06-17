1770a91c067a6288f95a26ce6a6369c39ae3497f
ChrisHal1 <c.halderkisslegg@web.de>
2015-06-17 19:50:39 +0200

    up0

--- src/HalloWelt.java
+++ src/HalloWelt.java
@@ -4,6 +4,7 @@ public class HalloWelt {
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	System.out.println("Hallo welt");
+	System.out.println("Hallo welt");
 
     }
 
