1c1730037ef1e770d05925f4e076aaf9fd2160a4
ChrisHal1 <c.halderkisslegg@web.de>
2015-06-17 20:41:02 +0200

    int i

--- src/HalloWelt.java
+++ src/HalloWelt.java
@@ -4,7 +4,8 @@ public class HalloWelt {
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	System.out.println("Hallo welt");
-	
+	//neuer branch
+	int i =0;
 	
 
     }
